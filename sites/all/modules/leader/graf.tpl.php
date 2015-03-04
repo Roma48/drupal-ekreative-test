74f8fc4be6ea327b9843bc922765c63ba2ac003f
Roma Paliy <romapaliy@Romas-Mac-mini.local>
2015-03-04 15:04:12 +0200

    my graph

---
+++ sites/all/modules/leader/graf.tpl.php
@@ -0,0 +1,42 @@
+
+<div style="width: 300px; height: 300px; border: 0px dotted blue; position: relative">
+    <div style="height: 300px; border-right: 1px solid blue; width: 25px">
+        <div style="position: absolute; width: 20px; bottom: 20px;">
+            <p style="width: 25px; border-top: 0px solid blue; height: 10px ">100</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">90</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">80</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">70</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">60</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">50</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">40</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">30</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">20</p>
+            <p style="width: 20px; border-top: 0px solid blue; height: 10px ">10</p>
+        </div>
+        <div style="display: block; width: 20px; height: <?php print $num['one'];   ?>px; position: absolute; bottom: 20px; left: 30px;  background-color: #000066"></div>
+        <div style="display: block; width: 20px; height: <?php print $num['two'];   ?>px; position: absolute; bottom: 20px; left: 60px;  background-color: #000066"></div>
+        <div style="display: block; width: 20px; height: <?php print $num['three']; ?>px; position: absolute; bottom: 20px; left: 90px;  background-color: #000066"></div>
+        <div style="display: block; width: 20px; height: <?php print $num['four'];  ?>px; position: absolute; bottom: 20px; left: 120px; background-color: #000066"></div>
+        <div style="display: block; width: 20px; height: <?php print $num['five'];  ?>px; position: absolute; bottom: 20px; left: 150px; background-color: #000066"></div>
+        <div style="display: block; width: 20px; height: <?php print $num['six'];   ?>px; position: absolute; bottom: 20px; left: 180px; background-color: #000066"></div>
+        <div style="display: block; width: 20px; height: <?php print $num['seven']; ?>px; position: absolute; bottom: 20px; left: 210px; background-color: #000066"></div>
+        <div style="display: block; width: 20px; height: <?php print $num['eight']; ?>px; position: absolute; bottom: 20px; left: 240px; background-color: #000066"></div>
+        <div style="display: block; width: 300px; height: 20px; position: absolute; bottom: 0; border-top: 1px solid blue">
+            <div style="width: 300px; height: 20px; float: left; margin-left: 30px">
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px; ">1</p>
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px; ">2</p>
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px; ">3</p>
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px; ">4</p>
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px">5</p>
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px">6</p>
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px">7</p>
+                <p style="display: table-cell; text-align: center; float: left; width: 20px; margin-right: 10px">8</p>
+            </div>
+    </div>
+</div>
+
+
+    
+
+
+
